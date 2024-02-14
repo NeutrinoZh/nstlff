2843052147782d9e033618860bab0297a59d5231
neutrinozh <neutrinozh@gmail.com>
2024-02-14 16:28:02 +0200

    added ruleset

--- src/rule.py
+++ src/rule.py
@@ -7,26 +7,24 @@ class Rule:
         self.file_naming = file_naming
         self.folder_naming = folder_naming
 
+class RuleSet:
+    def __init__(self, rules: set) -> None:
+        self.rules = rules
 
-rules = [
-    Rule(r".*", r"[a-z]*.test", r"[A-Z][a-zA-Z]*"),
-]
+    def checkEntry(self, name: str, abs_name: str, is_folder: bool) -> bool:
+        def checkName(rule: Rule) -> bool:
+            if is_folder:
+                return re.fullmatch(rule.folder_naming, name)
+            return re.fullmatch(rule.file_naming, name)
 
+        for rule in self.rules:
+            is_match = re.fullmatch(rule.pattern, abs_name)
+            if is_match:
+                is_match = checkName(rule)
+                if not is_match:
+                    return False
+                break
+        else:
+            return False
 
-def checkEntry(name: str, abs_name: str, is_folder: bool) -> bool:
-    def checkName(rule: Rule) -> bool:
-        if is_folder:
-            return re.fullmatch(rule.folder_naming, name)
-        return re.fullmatch(rule.file_naming, name)
-
-    for rule in rules:
-        is_match = re.fullmatch(rule.pattern, abs_name)
-        if is_match:
-            is_match = checkName(rule)
-            if not is_match:
-                return False
-            break
-    else:
-        return False
-
-    return True
+        return True
