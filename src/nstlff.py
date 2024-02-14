b1cfcf586672f20a12311ddb0a668435752e4023
neutrinozh <neutrinozh@gmail.com>
2024-02-14 16:28:17 +0200

    added some preparations

--- src/nstlff.py
+++ src/nstlff.py
@@ -1,19 +1,23 @@
 import os
 import rule
+from preparation import Preparation
 
+ruleset = rule.RuleSet([
+    rule.Rule(r".*", Preparation.camelCase, Preparation.pascalCase),
+])
 
 def main():
     for root, dirs, files in os.walk("./test/"):
         for folder in dirs:
             abs_path = os.path.join(root, folder)
-            if not rule.checkEntry(folder, abs_path, True):
+            if not ruleset.checkEntry(folder, abs_path, True):
                 print(
                     f"Folder '{folder}' in '{root}' does not match the specified pattern."
                 )
 
         for file in files:
             abs_path = os.path.join(root, file)
-            if not rule.checkEntry(file, abs_path, False):
+            if not ruleset.checkEntry(file, abs_path, False):
                 print(
                     f"File '{file}' in '{root}' does not match the specified pattern."
                 )
---
+++ src/preparation.py
@@ -0,0 +1,5 @@
+from rule import *
+
+class Preparation:
+    camelCase = r"[a-z][a-z0-9_]*"
+    pascalCase = r"[A-Z][a-z0-9_]*"
